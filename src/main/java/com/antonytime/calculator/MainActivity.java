7536b624f1bd11e6121cd757295b074c0c1ce950
AntonyTime <Y.AntNik@Gmail.com>
2014-11-18 19:31:54 +0300

    init commit

---
+++ src/main/java/com/antonytime/calculator/MainActivity.java
@@ -0,0 +1,87 @@
+package com.antonytime.calculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+
+    private EditText scr;
+    private float numberBf;
+    private String operation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+
+        scr = (EditText) findViewById(R.id.display);
+    }
+
+    public void mMath(String str){
+        numberBf = Float.parseFloat(scr.getText().toString());
+        operation = str;
+        scr.setText("0");
+    }
+
+    public void mResult(){
+        float numAf = Float.parseFloat(scr.getText().toString());
+        float result = 0;
+
+        if(operation.equals("+")){
+            result = numAf + numberBf;
+        }
+        if(operation.equals("-")){
+            result = numberBf - numAf;
+        }
+        if(operation.equals("*")){
+            result = numAf * numberBf;
+        }
+        if(operation.equals("/")){
+            result = numberBf / numAf;
+        }
+        scr.setText(String.valueOf(result));
+    }
+
+    private void getKeyboard(String str){
+        String scrCurrent = scr.getText().toString();
+        if(scrCurrent.equals("0"))
+            scrCurrent = "";
+        scrCurrent += str;
+        scr.setText(scrCurrent);
+    }
+
+    public void onClick(View view) {
+
+        switch (view.getId()){
+            case R.id.btnC:
+                scr.setText("0");
+                numberBf = 0;
+                operation = "";
+                break;
+            case R.id.btnPlus:
+                mMath("+");
+                break;
+            case R.id.btnSubtract:
+                mMath("-");
+                break;
+            case R.id.btnMultiply:
+                mMath("*");
+                break;
+            case R.id.btnDivide:
+                mMath("/");
+                break;
+            case R.id.btnEqual:
+                mResult();
+                break;
+            default:
+                String numb = ((Button) view).getText().toString();
+                getKeyboard(numb);
+                break;
+            }
+        }
+    }
+
+
